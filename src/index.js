269d0fca48a857fb806c0d63879874a276296fa6
VikiMiki <vikimikirc@gmail.com>
2019-10-02 23:28:17 +0300

    add function Multiply

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 module.exports = function multiply(first, second) {
   // your solution
+  let a=BigInt(first);
+  let b=BigInt(second);
+  let sum=0;
+  //BigInt
+  sum=(a*b);
+  return (String(sum));
 }
